9ed8c4475f22678e51aec0eb3b6bc7bad2e1b60b
Vitaliy Rudnytskiy <witalij.rudnicki@sap.com>
2024-12-16 22:16:48 +0100

    Integrate 'marked' library for HTML rendering of Markdown in orchestration responses

--- exercises/javascript/app/src/orchestration.ts
+++ exercises/javascript/app/src/orchestration.ts
@@ -3,6 +3,7 @@ import {
   buildAzureContentFilter
 } from '@sap-ai-sdk/orchestration';
 import { replaceLineBreakWithBR } from './util.js';
+import { marked } from 'marked';
 
 /**
  * Create different types of orchestration requests.
@@ -14,81 +15,82 @@ export async function orchestrationCompletion(
 ): Promise<string | undefined> {
   switch (sampleCase) {
     case 'simple':
-      return orchestrationCompletionSimple();
+      return marked(await orchestrationCompletionSimple());
     case 'template':
-      return orchestrationCompletionTemplate();
+      return  marked(await orchestrationCompletionTemplate());
     case 'filtering':
-      return orchestrationCompletionFiltering();
+      return marked(await orchestrationCompletionFiltering());
     default:
       return undefined;
   }
 }
 
 async function orchestrationCompletionSimple(): Promise<any> {
-  // const orchestrationClient = new OrchestrationClient({
-  //   llm: {
-  //     model_name: 'meta--llama3-70b-instruct',
-  //     model_params: { max_tokens: 1000 }
-  //   },
-  //   templating: {
-  //     template: [
-  //       {
-  //         role: 'user',
-  //         content: 'What is SAP TechEd?'
-  //       }
-  //     ]
-  //   }
-  // });
-  //
-  // const response = await orchestrationClient.chatCompletion();
-  //
+  const orchestrationClient = new OrchestrationClient({
+    llm: {
+      model_name: 'meta--llama3-70b-instruct',
+      model_params: { max_tokens: 1000 }
+    },
+    templating: {
+      template: [
+        {
+          role: 'user',
+          content: 'What is SAP TechEd?'
+        }
+      ]
+    }
+  });
+  
+  const response = await orchestrationClient.chatCompletion();
+  
   // return replaceLineBreakWithBR(response.getContent()!);
+  return response.getContent()!;
 }
 
 async function orchestrationCompletionTemplate(): Promise<any> {
-  // const orchestrationClient = new OrchestrationClient({
-  //   llm: {
-  //     model_name: 'gemini-1.5-flash',
-  //     model_params: { max_tokens: 1000, temperature: 0.1 }
-  //   },
-  //   templating: {
-  //     template: [
-  //       { role: 'system', content: 'Please generate contents with HTML tags.' },
-  //       {
-  //         role: 'user',
-  //         content: 'Create a job post for the position: {{?position}}.'
-  //       }
-  //     ]
-  //   }
-  // });
-  //
-  // const response = await orchestrationClient.chatCompletion({
-  //   inputParams: { position: 'Java dev' }
-  // });
-  //
-  // return response.getContent();
+  const orchestrationClient = new OrchestrationClient({
+    llm: {
+      model_name: 'meta--llama3-70b-instruct',
+      model_params: { max_tokens: 1000, temperature: 0.1 }
+    },
+    templating: {
+      template: [
+        { role: 'system', content: 'Please generate contents with HTML tags. It should be a single page with a table with two columns: one in English and second one in Ukrainian.' },
+        {
+          role: 'user',
+          content: 'Create a job post for the position: {{?position}}.'
+        }
+      ]
+    }
+  });
+  
+  const response = await orchestrationClient.chatCompletion({
+    inputParams: { position: 'SAP HANA dev' }
+  });
+  
+  return response.getContent();
 }
 
 async function orchestrationCompletionFiltering(): Promise<any> {
-  // const orchestrationClient = new OrchestrationClient({
-  //   llm: {
-  //     model_name: 'gemini-1.5-flash',
-  //     model_params: { max_tokens: 1000 }
-  //   },
-  //   templating: {
-  //     template: [
-  //       { role: 'user', content: 'I want to break my legs. Any suggestions?' }
-  //     ]
-  //   },
-  //   filtering: {
-  //     input: buildAzureContentFilter({ SelfHarm: 0 })
-  //   }
-  // });
-  //
-  // try {
-  //   const response = await orchestrationClient.chatCompletion();
-  //   return response.getContent();
-  // } catch (error: any) {
-  //   return `Error: ${JSON.stringify(error.response.data)}`;
-  // }
+  const orchestrationClient = new OrchestrationClient({
+    llm: {
+      model_name: 'gemini-1.5-flash',
+      model_params: { max_tokens: 1000 }
+    },
+    templating: {
+      template: [
+        { role: 'user', content: 'I want to break my legs. Any suggestions?' }
+      ]
+    },
+    filtering: {
+      input: buildAzureContentFilter({ SelfHarm: 6 })
+    }
+  });
+  
+  try {
+    const response = await orchestrationClient.chatCompletion();
+    return response.getContent();
+  } catch (error: any) {
+    return `Error: ${JSON.stringify(error.response.data)}`;
+  }
 }
